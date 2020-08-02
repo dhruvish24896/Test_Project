e8dc6c226af6291e27cb1cd6966dce27029e94ca
dhru20 <sunny24896@gmail.com>
2020-08-02 10:03:04 +0530

    first git push commit

--- fucntion_1.c
+++ fucntion_1.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-
+#define SYSTEM 20
 int main()
 {
+	int a = 20;
+
+#if SYSTEM == a
+	printf("Hello");
+#endif
+
+#if SYSTEM == 20
+	printf("Wrold\n");
+#endif
+
 	printf("in function_1 for first commit \r\n ");
 	return 0;
 }
